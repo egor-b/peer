c4361fa7875d79b41f1ea980d34144418c93c744
ealeksa2 <ealeksa2@ford.com>
2023-05-09 09:19:37 -0400

    Initial Commit

---
+++ peer/peerApp.swift
@@ -0,0 +1,17 @@
+//
+//  peerApp.swift
+//  peer
+//
+//  Created by Bryzgalov, Egor (A.) on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct peerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
