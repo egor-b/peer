166344596841fe3636cce3a058e354ab9a499d1a
Egor Bryzgalov <ealeksa2@ford.com>
2023-05-11 09:26:28 -0400

    add all colors

--- peer/extensions/Color.swift
+++ peer/extensions/Color.swift
@@ -11,4 +11,7 @@ import SwiftUI
 extension Color {
     static let background = Color(red: 119/255, green: 108/255, blue: 255/255)
     static let progress = Color(red: 130/255, green: 220/255, blue: 251/255)
+    static let darkPurple = Color(red: 63/255, green: 53/255, blue: 239/255)
+    static let dark = Color(red: 0/255, green: 19/255, blue: 47/355)
+    static let highRiseWhite = Color(red: 255/255, green: 255/255, blue: 254/255)
 }
